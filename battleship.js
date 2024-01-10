f6089dddf380ab7ed65858a81d7852950888ff28
Matt Berger <bergermd1@gmail.com>
2024-01-09 21:07:35 -0500

    Add console display and includesCoordinates functions

--- battleship.js
+++ battleship.js
@@ -20,20 +20,18 @@ function Ship(coords) {
     }
 }
 
-//5: carrier
-//4: battleship
-//3: submarine
-//3: cruiser
-//2: destroyer
-
 function Gameboard() {
     const ships = {};
+    let allShipCoords = [];
     let missCoords = [];
     let hitCoords = [];
 
     function initializeShips(shipsCoords) {
         for (const ship in shipsCoords) {
             ships[ship] = Ship(shipsCoords[ship]);
+            shipsCoords[ship].forEach(coord => {
+                allShipCoords.push(coord);
+            });
         }
     }
 
@@ -65,17 +63,70 @@ function Gameboard() {
         return sunk;
     }
 
+    function includesCoords(arr, coords) {
+        // console.log(arr[0][0] === coords[0] && arr[0][1] === coords[1]);
+        // console.log(arr[0][1] === coords[1]);
+        let includes = false;
+        arr.forEach(element => {
+            // console.log(element);
+            if (element[0] === coords[0] && element[1] === coords[1]) {
+                // console.log('yeah');
+                includes = true;
+            }
+        })
+        return includes;
+    }
+
     // function isValidLocation();
+
+    function displayInConsole() {
+        let str = '';
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                if (includesCoords(this.hitCoords, [j,i])) {
+                    str += 'H';//hit
+                } else if (includesCoords(this.missCoords, [j,i])) {
+                    str += 'M';//miss
+                } else if (includesCoords(this.allShipCoords, [j,i])) {
+                    str += 'O';//boat
+                } else {
+                    str += '-';//water (empty)
+                }
+            }
+            str += '\n';
+        }
+        console.log(str);
+    }
 
     return {
         ships,
+        allShipCoords,
         missCoords,
         hitCoords,
         initializeShips,
+        includesCoords,
         receiveAttack,
         allShipsSunk,
+        displayInConsole,
     }
 }
+
+let g = Gameboard();
+// console.log(g.allShipCoords);
+g.initializeShips({
+    'carrier': [[0,0], [1,0], [2,0], [3,0], [4,0]],
+    'battleship': [[0,2], [1,2], [2,2], [3,2]],
+    'submarine': [[0,4], [1,4], [2,4]],
+    'cruiser': [[0,6], [1,6], [2,6]],
+    'destroyer': [[0,8], [1,8]],
+})
+// console.log(g.includesCoords(g.allShipCoords, [0,0]));
+g.displayInConsole();
+g.receiveAttack([0,0])
+g.receiveAttack([1,0])
+g.receiveAttack([2,0])
+g.receiveAttack([8,0])
+g.displayInConsole();
 
 module.exports = {
     Ship,
