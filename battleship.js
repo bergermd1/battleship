b2472c26de46b929376faf4d8487748cc8ca2b22
Matt Berger <bergermd1@gmail.com>
2024-01-14 15:47:47 -0500

    Delete commented code.

--- battleship.js
+++ battleship.js
@@ -248,7 +248,6 @@ function Player(myBoard, enemyBoard, myOpponent, myTurn, isHuman) {
         }
         console.log(coords);
         if (this.enemyBoard.receiveAttack(coords)) {
-            // console.log('yeah');
             repaintSquareHit(this.myBoard.boardNumber, this.enemyBoard.boardNumber, coords);
         } else {
             repaintSquareMiss(this.myBoard.boardNumber, this.enemyBoard.boardNumber, coords);
@@ -282,26 +281,8 @@ let board2 = Gameboard(2);
 let coords1 = board1.getAllRandomShipCoords();
 let coords2 = board2.getAllRandomShipCoords();
 // console.log(coords);
-board1.initializeShips(
-    coords1
-    // {
-        // 'carrier': [[0,0], [1,0], [2,0], [3,0], [4,0]],
-        // 'battleship': [[0,2], [1,2], [2,2], [3,2]],
-        // 'submarine': [[0,4], [1,4], [2,4]],
-        // 'cruiser': [[0,6], [1,6], [2,6]],
-        // 'destroyer': [[0,8], [1,8]],
-    // }
-)
-board2.initializeShips(
-    coords2
-    // {
-        // 'carrier': [[5,1], [6,1], [7,1], [8,1], [9,1]],
-        // 'battleship': [[6,3], [7,3], [8,3], [9,3]],
-        // 'submarine': [[7,5], [8,5], [9,5]],
-        // 'cruiser': [[7,7], [8,7], [9,7]],
-        // 'destroyer': [[8,9], [9,9]],
-    // }
-)
+board1.initializeShips(coords1)
+board2.initializeShips(coords2)
 
 // gameLoop();
 
@@ -317,49 +298,6 @@ function gameLoop() {
     // board2.displayFullBoard();
     colorShipSquares(board1.boardNumber, board1.allShipCoords);
     colorShipSquares(board2.boardNumber, board2.allShipCoords);
-
-    // setInterval(() => {
-    //     p1.randomAttack();
-    //     if (p1.enemyBoard.allShipsSunk()) {
-    //         return;
-    //     }
-    //     // let move2 = prompt("Enter move for player 2");
-    //     p2.randomAttack();
-    //     if (p2.enemyBoard.allShipsSunk()) {
-    //         return;
-    //     }
-    //     console.log('Player 1');
-    //     p1.myBoard.displayFullBoard();
-    //     p1.enemyBoard.displayHitsMisses();
-    //     console.log('Player 2');
-    //     p2.myBoard.displayFullBoard();
-    //     p2.enemyBoard.displayHitsMisses();
-    // }, 500);
-
-    // while (!p1.enemyBoard.allShipsSunk() || !p2.enemyBoard.allShipsSunk()) {
-
-    // }
-
-    // for (let i = 0; i < 1; i ++) {
-        // let move1 = prompt("Enter move for player 1");
-        // p1.attack(move1);
-        // p1.randomAttack();
-        // if (p1.enemyBoard.allShipsSunk()) {
-            // break;
-        // }
-        // let move2 = prompt("Enter move for player 2");
-        // p2.attack(move2);
-        // p2.randomAttack();
-        // if (p2.enemyBoard.allShipsSunk()) {
-            // break;
-        // }
-        // console.log('Player 1');
-        // p1.myBoard.displayFullBoard();
-        // p1.enemyBoard.displayHitsMisses();
-        // console.log('Player 2');
-        // p2.myBoard.displayFullBoard();
-        // p2.enemyBoard.displayHitsMisses();
-    // }
 }
 
 function getRandomCoords() {
@@ -373,93 +311,23 @@ function getRandomDirection() {
 }
 
 function colorShipSquares(boardNumber, coords) {
-    // console.log(this);
     coords.forEach(coord => {
-        // console.log(document.querySelector(`.gridSquare`));
-        // document.querySelector(`#gameBoardP${boardNumber}-${coord[0]}${coord[1]}`).classList.add('shipSquare');
         document.querySelector(`#gameBoardP${boardNumber}-${coord[0]}${coord[1]}`).classList.add('unhitShipSquare');
     });
 }
 
 function repaintSquareHit(boardNumber, enemyBoardNumber, coord) {
-    // console.log(`#gameboardP${enemyBoardNumber}-${coord[0]}${coord[1]}`);
     document.querySelector(`#hitMissP${boardNumber}-${coord[0]}${coord[1]}`).classList.remove('unhitShipSquare');
     document.querySelector(`#hitMissP${boardNumber}-${coord[0]}${coord[1]}`).classList.add('hitShipSquare');
     document.querySelector(`#gameBoardP${enemyBoardNumber}-${coord[0]}${coord[1]}`).classList.remove('unhitShipSquare');
     document.querySelector(`#gameBoardP${enemyBoardNumber}-${coord[0]}${coord[1]}`).classList.add('hitShipSquare');
 }
 function repaintSquareMiss(boardNumber, enemyBoardNumber, coord) {
-    // console.log(`#gameboardP${enemyBoardNumber}-${coord[0]}${coord[1]}`);
     document.querySelector(`#hitMissP${boardNumber}-${coord[0]}${coord[1]}`).classList.remove('unhitShipSquare');
     document.querySelector(`#hitMissP${boardNumber}-${coord[0]}${coord[1]}`).classList.add('missShipSquare');
     document.querySelector(`#gameBoardP${enemyBoardNumber}-${coord[0]}${coord[1]}`).classList.remove('unhitShipSquare');
     document.querySelector(`#gameBoardP${enemyBoardNumber}-${coord[0]}${coord[1]}`).classList.add('missShipSquare');
 }
-
-// function initializeHTML() {
-//     const hitMiss = document.createElement('div');
-//     hitMiss.className = 'hitMiss grid';
-//     const gameBoard = document.createElement('div');
-//     gameBoard.className = 'gameBoard grid';
-//     for (let i = 0; i < 10; i++) {
-//         const hitMissRow = document.createElement('div');
-//         hitMissRow.className = 'gridRow';
-//         hitMiss.appendChild(hitMissRow)
-        
-//         const gameBoardRow = document.createElement('div');
-//         gameBoardRow.className = 'gridRow';
-//         gameBoard.appendChild(gameBoardRow);
-        
-//         for (let j = 0; j < 10; j++) {
-//             const hitMissGridSquare = document.createElement('div');
-//             hitMissGridSquare.className = 'gridSquare';
-//             hitMissGridSquare.id = `hitMissP1-${i}${j}`;
-//             // gridSquare.addEventListener('click', )
-//             hitMissRow.appendChild(hitMissGridSquare);
-            
-//             const gameBoardGridSquare = document.createElement('div');
-//             gameBoardGridSquare.className = 'gridSquare';
-//             gameBoardGridSquare.id = `gameBoardP1-${i}${j}`;
-//             gameBoardRow.appendChild(gameBoardGridSquare);
-//         }
-//     }
-
-//     const hitMiss2 = document.createElement('div');
-//     hitMiss2.className = 'hitMiss grid';
-//     const gameBoard2 = document.createElement('div');
-//     gameBoard2.className = 'gameBoard grid';
-//     for (let i = 0; i < 10; i++) {
-//         const hitMissRow = document.createElement('div');
-//         hitMissRow.className = 'gridRow';
-//         hitMiss2.appendChild(hitMissRow)
-        
-//         const gameBoardRow = document.createElement('div');
-//         gameBoardRow.className = 'gridRow';
-//         gameBoard2.appendChild(gameBoardRow);
-        
-//         for (let j = 0; j < 10; j++) {
-//             const hitMissGridSquare = document.createElement('div');
-//             hitMissGridSquare.className = 'gridSquare';
-//             hitMissGridSquare.id = `hitMissP2-${i}${j}`;
-//             gridSquare.addEventListener('click', () => {
-                
-//             })
-//             hitMissRow.appendChild(hitMissGridSquare);
-            
-//             const gameBoardGridSquare = document.createElement('div');
-//             gameBoardGridSquare.className = 'gridSquare';
-//             gameBoardGridSquare.id = `gameBoardP2-${i}${j}`;
-//             gameBoardRow.appendChild(gameBoardGridSquare);
-//         }
-//     }
-
-//     // console.log(document.querySelector('.playerScreen'));
-//     document.querySelector('.playerScreen').appendChild(hitMiss);
-//     document.querySelector('.playerScreen').appendChild(gameBoard);
-//     document.querySelector('.computerScreen').appendChild(hitMiss2);
-//     document.querySelector('.computerScreen').appendChild(gameBoard2);
-//     // return hitMiss;
-// }
 
 module.exports = {
     Ship,
